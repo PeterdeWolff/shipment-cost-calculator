651012c398fb95e803cf4f3066775511683a58e3
sWimmie <wim@swimmie.nl>
2019-11-27 08:32:34 +0100

    Comments to get you going!
    Added some pseudo code to get you started on your way

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -18,6 +18,14 @@ $( document ).ready(function() {
 
 });
 
+//on button click there must be a function that calculates all the props at once
+// function(){ var length = $("#length).val(); for every var the same only dropdown is not val() but attr();
+// calculate the volume_weight => var volume_weight = (length * width * height)/5000
+// calculate weight => var weight = lenght * width * height and than something or else this is always higher?? 
+// check if (volume_weight > weight) { check inside json for the volume_weight and code from the dropdown } else { check inside json for weight and code from dropdown}
+// in both cases return the value and done!
+
+
 var lenght();
 var width ();
 var height();
