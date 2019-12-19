af8e39e142e0c70e2a81d39273a0f8efebaf86f5
Ubuntu <ubuntu@ip-172-31-89-176.ec2.internal>
2019-12-19 18:56:47 +0000

    defined rules for calculation

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -19,6 +19,9 @@ $.getJSON("assets/static/data/csvjson.json", function (cost){
     costData = cost;
 }, function(succes) {});
 
+var dimension;
+var calculatedWeight;
+
 function calculate(){
     
    var lenght = parseInt($("#lenght").val());
@@ -26,20 +29,66 @@ function calculate(){
     var height = parseInt($("#height").val());
     var weight = Math.ceil(parseInt($("#weight").val()));
     var country = $("#countries-dropdown").val();
-    var urlString = 'assets/static/data/csvjson.json';
     var volume_weight = Math.ceil((lenght * width * height) /5000);
-
-   
         
+      
         if (volume_weight > weight) {
-          console.log(costData);  
+            calcDimension(lenght, width, height);
+            calcWeight(volume_weight, country);
+         
         
         } else {
+            calcDimension(lenght, width, height);
+           calcWeight(weight, country)
             
         }
 }
 
+function clearAll () {
+    $("#lenght").val("");
+    $("#width").val("");
+    $("#height").val("");
+    $("#weight").val("");
+    $("#countries-dropdown").val("Choose your country");
+    $('#shipping_costs').val("");
+    
+}
 
+
+function calcDimension (l,w,h) {
+    if (l > w && l > h){dimension = 2*w + 2*h + l;
+    }
+    else if (l < w && l > h) {dimension = 2*l + 2*h + w;
+    }
+    else if (w > l && l < h) {dimension = 2*l +2*w + h;
+    }
+    
+    if (dimension > 400){
+        alert("dimension is too big");
+    return
+    }
+}
+
+function calcWeight (w, c) {
+    if (w <= 10) {
+        calculatedWeight = w;
+    } 
+    else if (w > 10 && w <= 50) {
+        calculatedWeight = 2*(Math.round(w) /2);
+    }
+    else if (w > 50 && w <= 70) {
+        calculatedWeight = 5*(Math.round(w) /5);
+    } else {
+       alert("Weight is over 70kg");
+       return
+    }
+    
+    
+          var d = costData[calculatedWeight];
+          var cost = d[c];
+          $('#shipping_costs').val(cost);
+    
+}
 
 
 
